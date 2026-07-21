ab3790320fdbcdfca0f6225b1c1c5533643158e0
wareya <wareya@gmail.com>
2026-07-21 18:55:55 -0400

    rename project gmksplit folder to make it easier to autocomplete differently from the gm81 file

--- folder_to_gm81.bat
+++ folder_to_gm81.bat
@@ -1,2 +1,2 @@
-gmksplit --preserve-ids all --allow-duplicate-ids tiles,instances DT3_v1_5_2_4_v120_gmksplit DT3_v1.5.2.4_v120_conv.gm81
+gmksplit --preserve-ids all --allow-duplicate-ids tiles,instances fDT3_v1_5_2_4_v120_gmksplit DT3_v1.5.2.4_v120_conv.gm81
 pause
